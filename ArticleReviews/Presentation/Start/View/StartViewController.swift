f0abf4f5b2ef410eb7d0ff958784e395e39384da
mariimeireles <marianaribeiromeireles@gmail.com>
2018-07-25 19:39:40 +0200

    selection view

--- ArticleReviews/Presentation/Start/View/StartViewController.swift
+++ ArticleReviews/Presentation/Start/View/StartViewController.swift
@@ -3,10 +3,24 @@
 import UIKit
 
 final class StartViewController: UIViewController {
-
+    
+    private let selectionViewModel: ArticlesViewModel
+    
+    init(selectionViewModel: ArticlesViewModel) {
+        self.selectionViewModel = selectionViewModel
+        super.init(nibName: StartViewController.identifier, bundle: Bundle.main)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     @IBAction func startButton(_ sender: Any) {
+        let selectionViewController = SelectionViewController(viewModel: selectionViewModel)
+        navigationController?.pushViewController(selectionViewController, animated: true)
     }
 }
