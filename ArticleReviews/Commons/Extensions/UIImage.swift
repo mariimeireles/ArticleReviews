66125939caa9323388a473513cf314dfb72b9503
mariimeireles <marianaribeiromeireles@gmail.com>
2018-07-26 23:33:51 +0200

    uiimage extension

--- ArticleReviews/Commons/Extensions/UIImage.swift
+++ ArticleReviews/Commons/Extensions/UIImage.swift
@@ -8,4 +8,9 @@ extension UIImage {
         guard let data = try? Data(contentsOf: url), let image = UIImage(data: data) else { return UIImage() }
         return image
     }
+    
+    func load(data: Data) -> UIImage {
+        guard let image = UIImage(data: data) else { return UIImage() }
+        return image
+    }
 }
---
+++ ArticleReviews/Presentation/Selection/View/ReviewButtonState.swift
@@ -0,0 +1,9 @@
+//
+//  ReviewButtonState.swift
+//  ArticleReviews
+//
+//  Created by Mariana Meireles on 26/07/18.
+//  Copyright © 2018 Mariana Meireles. All rights reserved.
+//
+
+import Foundation
