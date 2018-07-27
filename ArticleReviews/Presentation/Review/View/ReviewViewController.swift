4b1899dabeac241f58ea41917b6514b0802d8d93
mariimeireles <marianaribeiromeireles@gmail.com>
2018-07-27 17:29:02 +0200

    review view controller

--- ArticleReviews/Presentation/Review/View/ReviewViewController.swift
+++ ArticleReviews/Presentation/Review/View/ReviewViewController.swift
@@ -1,35 +1,58 @@
-//
-//  ReviewViewController.swift
-//  ArticleReviews
-//
-//  Created by Mariana Meireles on 27/07/18.
-//  Copyright © 2018 Mariana Meireles. All rights reserved.
-//
+
 
 import UIKit
 
-class ReviewViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+final class ReviewViewController: UIViewController {
+    
+    @IBOutlet private var listView: ListView!
+    @IBOutlet private var gridView: GridView!
+    @IBOutlet private var listButton: UIButton!
+    @IBOutlet private var gridButton: UIButton!
+    private let viewModel: ReviewViewModel
+    
+    init(viewModel: ReviewViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: ReviewViewController.identifier, bundle: Bundle.main)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setScreenState()
+    }
+    
+    private func setScreenState() {
+        let screenState = viewModel.retriveReviewedArticlesFromCoreData()
+        render(screenState)
+    }
+    
+    private func render(_ state: ReviewScreenState) {
+        switch state {
+        case let .success(articleEntities):
+            listView.articleEntities = articleEntities
+            gridView.articleEntities = articleEntities
+        case .failure:
+            let alert = UIAlertController(title: "Something went wrong.", message: "Sorry, it's not you, it's us! Please try again later.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func listButton(_ sender: Any) {
+        showGridView(false)
+    }
+    
+    @IBAction func gridButton(_ sender: Any) {
+        showGridView(true)
+    }
+    
+    private func showGridView(_ bool: Bool) {
+        listView.alpha = bool ? 0 : 1
+        listButton.alpha = bool ? 0.5 : 1
+        gridView.alpha = bool ? 1 : 0
+        gridButton.alpha = bool ? 1 : 0.5
+    }
 }
